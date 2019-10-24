1cc38db214d91e575214588da163492adc79f60c
Puming <pmchongqing@163.com>
2019-10-24 12:46:52 +0800

    add cadre

---
+++ app/src/main/java/com/pm/an/MainActivity.java
@@ -0,0 +1,17 @@
+package com.pm.an;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author pm
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
